ca5ed55fef5316935a6e73bc0f35e39fbe3f9c54
Michael Parker <docmparker@gmail.com>
2024-09-23 16:11:15 -0400

    Fix prompts.py to properly use passed context dict in 09_custom_model_graded_prompt_foo

--- prompt_evaluations/09_custom_model_graded_prompt_foo/prompts.py
+++ prompt_evaluations/09_custom_model_graded_prompt_foo/prompts.py
@@ -1,14 +1,16 @@
-def basic_summarize(article):
-  return f"Summarize this article {article}"
+def basic_summarize(context: dict):
+  variables = context['vars']
+  return f"Summarize this article {variables['article']}"
 
-def better_summarize(article):
-  return f"""
-  Summarize this article for a grade-school audience: {article}"""
+def better_summarize(context: dict):
+  variables = context['vars']
+  return f"""Summarize this article for a grade-school audience: {variables['article']}"""
 
-def best_summarize(article):
-  return f"""
-  You are tasked with summarizing long wikipedia articles for a grade-school audience.
-  Write a short summary, keeping it as concise as possible. 
-  The summary is intended for a non-technical, grade-school audience. 
-  This is the article: {article}"""
+def best_summarize(context: dict):
+  variables = context['vars']
+  return f"""\
+You are tasked with summarizing long wikipedia articles for a grade-school audience.
+Write a short summary, keeping it as concise as possible. 
+The summary is intended for a non-technical, grade-school audience. 
+This is the article: {variables['article']}"""
 
